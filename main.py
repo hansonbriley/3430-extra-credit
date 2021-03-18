0edec90f3acd654077178651fec87c65d1a514a4
Briley Hanson <hansonb7@spu.edu>
2021-03-17 22:10:31 -0700

    renamed variables

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import random
 
-
 # Generate a list of 30 houses
-# Numbers represent distance from eastern endpoint
+# Random numbers represent distance from eastern endpoint
 house_list = []
 for i in range(0, 30):
     house_list.append(random.randint(1,99))
-# sort houses by closest to eastern endpoint
-house_list = sorted(house_list)
-# place first base 4 miles west of eastern most house
-base = house_list[0] + 4
-num_bases = 1
 
-# if mile marker of current house - mile marker of current base > 4
-# add a new base 4 miles west
+# sort houses by distance from eastern endpoint
+house_list = sorted(house_list)
+
+# place first base station 4 miles west of eastern most house
+base_station = house_list[0] + 4
+num_base_stations = 1
+
+# if current house is greater than 4 miles from the last placed base station
+# add a new base station 4 miles west of current house
 for i in range(1, len(house_list)):
-    if abs(base - house_list[i]) > 4:
-        base = house_list[i] + 4
-        num_bases += 1
-print("Towers needed: {}".format(num_bases))
+    if abs(base_station - house_list[i]) > 4:
+        base_station = house_list[i] + 4
+        num_base_stations += 1
+        
+print("Base stations needed: {}".format(num_base_stations))
 
